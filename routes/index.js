673db7d361c59660c5a8e9d83417059c76ce5361
Chilka <S530712@nwmissouri.edu>
2018-09-12 16:48:03 -0500

    added routes, modified views and modularized project

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,29 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET officers page. */
+router.get('/officers', function(req, res, next) {
+  res.render('officers', { title: 'Express' });
+});
+
+/* GET 13 Universities page page. */
+router.get('/13universities', function(req, res, next) {
+  res.render('13universities', { title: 'Express' });
+});
+
+/* GET designated voters page. */
+router.get('/designated_voters', function(req, res, next) {
+  res.render('designated_voters', { title: 'Express' });
+});
+
+/* GET meetings page. */
+router.get('/meetings', function(req, res, next) {
+  res.render('meetings', { title: 'Express' });
+});
+
+/* GET report page. */
+router.get('/report', function(req, res, next) {
+  res.render('report', { title: 'Express' });
+});
+
 module.exports = router;
---
+++ views/templates/footer.ejs
@@ -0,0 +1,9 @@
+    <!-- Footer -->
+    <footer style="margin-top:2em; text-align:center" class="footer">
+        <div class="container">
+            <span class="text-muted">Missouri Association of Faculty Senates</span>
+        </div>
+    </footer>
+</body>
+
+</html>
